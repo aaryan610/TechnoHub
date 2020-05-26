411e6c84cfed70bebe78449d1b5ffdd057b68ca7
aaryan610 <65252264+aaryan610@users.noreply.github.com>
2020-05-26 23:42:23 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
 import React from "react";
-import technoLogo from "./images/logo.png";
+import technoLogo from "./images/technoLogo.png";
+import bitLogo from "./images/bitLogo.png";
 
 function Header() {
   return (
     <div>
-      <div className="heading">
+      <div className="logosCont">
         <img
-          src={technoLogo}
-          alt="Not available at the moment!!!"
-          className="logo"
+          src={bitLogo}
+          alt="Bhilai Institute of Technology, Durg"
+          className="logoLeft"
         />
-        <h2>Aptitude Test</h2>
-        <h3>
-          By Training & Placement Cell
-          <br />
-          BIT Durg
-        </h3>
+        <img src={technoLogo} alt="TechnoHub, BIT Durg" className="logoRight" />
+      </div>
+      <div className="heading">
+        <h2>Ek Bharat Shreshtha Bharat</h2>
+        <h3>Online Quiz Competition</h3>
         <br />
       </div>
       <br />
